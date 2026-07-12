4316f5000b2d684ee96c48224a0487710e810682
Jairus <santosjirro55@gmail.com>
2026-07-12 17:05:31 +0800

    Added frontend folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import {app} from './server.ts';
 import {env} from '../env.ts';
 //passen port
+
+app.get(`/`, (req, res) => {
+    res.sendFile(process.cwd() + `/frontend/index.html`)
+});
+
 app.listen(env.PORT, () => {
-    console.log(`Server is running on port ${env.PORT}`);
+    console.log(`Server is running on http://localhost:${env.PORT}`);
 });
